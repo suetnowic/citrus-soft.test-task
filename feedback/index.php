0292d979aadd030d8ff0ce4b19657f64b2cfb500
Viktor Suetnov <57311061+suetnowic@users.noreply.github.com>
2024-01-12 10:15:32 +0300

    Update index.php

--- feedback/index.php
+++ feedback/index.php
@@ -1,6 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Feedback");
+$APPLICATION->SetTitle("Форма обратной связи");
 ?><?$APPLICATION->IncludeComponent(
 	"bitrix:main.feedback",
 	"",
