a6ff6bfe942a6258b84f23f6eded7fbcd7d47b8a
Viktor Suetnov <57311061+suetnowic@users.noreply.github.com>
2024-01-16 19:08:56 +0300

    Update events.php
    replaced #FORM_DATA# with #FORMDATA# in lines 3 and 4

--- local/php_interface/include/lang/ru/events.php
+++ local/php_interface/include/lang/ru/events.php
@@ -1,5 +1,5 @@
 <?php
 
-$MESS["USER_AUTH"] = "Пользователь авторизован: #ID#(#LOGIN#) #NAME#, данные из формы: #FORM_DATA#";
-$MESS["USER_NOT_AUTH"] = "Пользователь не авторизован, данные из формы: #FORM_DATA#";
+$MESS["USER_AUTH"] = "Пользователь авторизован: #ID#(#LOGIN#) #NAME#, данные из формы: #FORMDATA#";
+$MESS["USER_NOT_AUTH"] = "Пользователь не авторизован, данные из формы: #FORMDATA#";
 $MESS["REPLACE_DATA"] = "Замена данных в отсылаемом письме";
